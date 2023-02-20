de0ba7bf7e186929eef0d6ae332937a67aea4a96
romer8 <gio.busrom@gmail.com>
2023-02-20 12:30:48 -0700

    0.0.5

--- tethysapp/test2app/app.py
+++ tethysapp/test2app/app.py
@@ -1,10 +1,10 @@
 from tethys_sdk.base import TethysAppBase
+# from tethys_sdk.app_settings import CustomSetting
+from tethys_sdk.app_settings import CustomSimpleSetting
 
 
 class Test2App(TethysAppBase):
-    """
-    Tethys app class for Test2App.
-    """
+
 
     name = 'Test2App'
     description = ''
@@ -16,3 +16,61 @@ class Test2App(TethysAppBase):
     tags = ''
     enable_feedback = False
     feedback_emails = []
+
+    def custom_settings(self):
+        """
+        Example custom_settings method.
+        """
+        custom_settings = (
+
+            # CustomSetting(
+            #     name='default_name',
+            #     type=CustomSetting.TYPE_STRING,
+            #     description='Default model name.',
+            #     required=True
+            # ),
+            # CustomSetting(
+            #     name='max_count',
+            #     type=CustomSetting.TYPE_INTEGER,
+            #     description='Maximum allowed count in a method.',
+            #     required=False
+            # ),
+            # CustomSetting(
+            #     name='change_factor',
+            #     type=CustomSetting.TYPE_FLOAT,
+            #     description='Change factor that is applied to some process.',
+            #     required=True
+            # ),
+            # CustomSetting(
+            #     name='enable_feature',
+            #     type=CustomSetting.TYPE_BOOLEAN,
+            #     description='Enable this feature when True.',
+            #     required=True
+            # )
+            CustomSimpleSetting(
+                name='default_name',
+                type=CustomSimpleSetting.TYPE_STRING,
+                description='Default model name.',
+                required=True
+            ),
+            CustomSimpleSetting(
+                name='max_count',
+                type=CustomSimpleSetting.TYPE_INTEGER,
+                description='Maximum allowed count in a method.',
+                required=False
+            ),
+            CustomSimpleSetting(
+                name='change_factor',
+                type=CustomSimpleSetting.TYPE_FLOAT,
+                description='Change factor that is applied to some process.',
+                required=True
+            ),
+            CustomSimpleSetting(
+                name='enable_feature',
+                type=CustomSimpleSetting.TYPE_BOOLEAN,
+                description='Enable this feature when True.',
+                required=True
+            )
+        )
+
+        return custom_settings
